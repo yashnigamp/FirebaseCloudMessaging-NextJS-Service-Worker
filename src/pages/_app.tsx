2c51bfb60695e73a1059e808e74a03b14ecd8985
yashnigamp <yashnigam.p@gmail.com>
2024-05-13 14:19:58 +0530

    Project with service worker firebase

---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+import "../../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import { firebaseCloudMessaging } from "../config/firebase";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [fcmToken, setFcmToken] = useState<string | undefined>(undefined);
+
+  const getToken = async () => {
+    try {
+      const token = await firebaseCloudMessaging.init();
+      if (token) {
+        await firebaseCloudMessaging.getMessage();
+        setFcmToken(token);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.addEventListener("message", (event) =>
+        console.log("event for the service worker", event)
+      );
+    }
+    async function setToken() {
+      await getToken();
+    }
+    setToken();
+  }, []);
+
+  return <Component {...pageProps} fcmToken={fcmToken} getToken={getToken} />;
+}
+
+export default MyApp;
